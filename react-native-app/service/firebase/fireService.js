4523bcef4b00a665514aa335a9e3198ff0d4a146
Ging755 <denis.milic2@gmail.com>
2020-04-25 11:25:03 +0200

    firebase config

---
+++ react-native-app/service/firebase/fireService.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase';
+
+class Fire {
+  constructor() {
+    this.init();
+  }
+
+  init = () => {
+    try {
+      firebase.initializeApp({
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
+      });
+    } catch (err) {
+      // we skip the "already exists" message which is
+      // not an actual error when we're hot-reloading
+      if (!/already exists/.test(err.message)) {
+        console.error('Firebase initialization error', err.stack)
+      }
+    }
+  }
+}
+
+Fire = new Fire();
+export default Fire;
