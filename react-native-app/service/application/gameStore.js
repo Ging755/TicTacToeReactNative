e2001a14a8aa2bfa689d2b4ce0a5c7d063a0e7f4
Ging755 <denis.milic2@gmail.com>
2020-04-25 22:56:13 +0200

    minor fix when joining game

--- react-native-app/service/application/gameStore.js
+++ react-native-app/service/application/gameStore.js
@@ -19,18 +19,18 @@ class GameStore {
 
     @action.bound
     async upDateGrid(girdNumber) {
-        if(this.turn % 2 === 1 && this.player === "X"){
+        if (this.turn % 2 === 1 && this.player === "X") {
             this.grid.splice(girdNumber, 1, this.player);
 
-            FireService.firebase.database().ref('games/' + this.gameCode).set({
+            FireService.firebase.database().ref('games/' + this.gameCode + '/').set({
                 grid: this.grid,
                 timeStamp: FireService.firebase.database.ServerValue.TIMESTAMP
             })
         }
-        if(this.turn % 2 === 0 && this.player === "O"){
+        if (this.turn % 2 === 0 && this.player === "O") {
             this.grid.splice(girdNumber, 1, this.player);
 
-            FireService.firebase.database().ref('games/' + this.gameCode).set({
+            FireService.firebase.database().ref('games/' + this.gameCode + '/').set({
                 grid: this.grid,
                 timeStamp: FireService.firebase.database.ServerValue.TIMESTAMP
             })
@@ -55,73 +55,90 @@ class GameStore {
         this.player = "O";
         this.oponent = "X";
 
+        FireService.firebase.database().ref('games/' + this.gameCode + '/').once('value').then(snapshot => {
+            runInAction(() => {
+                this.grid = snapshot.val().grid;
+            })
+
+            this.grid.map((el) => {
+                if (el !== "") {
+                    this.turn++;
+                }
+            })
+            this.checkStatus();
+        });
         this.getAndTrackGame(this.gameCode);
     }
 
     async getAndTrackGame(gameKey) {
         FireService.firebase.database().ref('games/' + gameKey + '/').on('child_changed', snapshot => {
-            if(Array.isArray(snapshot.val())){
+            if (Array.isArray(snapshot.val())) {
+                console.log("Track", snapshot.val())
                 runInAction(() => {
                     this.grid = snapshot.val();
                 })
 
-                this.turn++;
+                this.grid.map((el) => {
+                    if (el !== "") {
+                        this.turn++;
+                    }
+                })
                 this.checkStatus();
             }
         });
     }
 
-    async checkStatus(){
+    async checkStatus() {
         let status = "";
-        if([this.grid[0], this.grid[1], this.grid[2]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[0] === this.player){
+        if ([this.grid[0], this.grid[1], this.grid[2]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[0] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[0] === this.oponent){
+            } else if (this.grid[0] === this.oponent) {
                 status = "YOU LOSE"
             }
-        }else if([this.grid[3], this.grid[4], this.grid[5]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[3] === this.player){
+        } else if ([this.grid[3], this.grid[4], this.grid[5]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[3] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[3] === this.oponent){
+            } else if (this.grid[3] === this.oponent) {
                 status = "YOU LOSE";
             }
-        }else if([this.grid[6], this.grid[7], this.grid[8]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[6] === this.player){
+        } else if ([this.grid[6], this.grid[7], this.grid[8]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[6] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[6] === this.oponent){
+            } else if (this.grid[6] === this.oponent) {
                 status = "YOU LOSE";
             }
-        }else if([this.grid[0], this.grid[4], this.grid[8]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[0] === this.player){
+        } else if ([this.grid[0], this.grid[4], this.grid[8]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[0] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[0] === this.oponent){
+            } else if (this.grid[0] === this.oponent) {
                 status = "YOU LOSE";
             }
-        }else if([this.grid[2], this.grid[4], this.grid[6]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[2] === this.player){
+        } else if ([this.grid[2], this.grid[4], this.grid[6]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[2] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[2] === this.oponent){
+            } else if (this.grid[2] === this.oponent) {
                 status = "YOU LOSE";
             }
-        }else if([this.grid[0], this.grid[3], this.grid[6]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[0] === this.player){
+        } else if ([this.grid[0], this.grid[3], this.grid[6]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[0] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[0] === this.oponent){
+            } else if (this.grid[0] === this.oponent) {
                 status = "YOU LOSE";
             }
-        }else if([this.grid[1], this.grid[4], this.grid[7]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[1] === this.player){
+        } else if ([this.grid[1], this.grid[4], this.grid[7]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[1] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[0] === this.oponent){
+            } else if (this.grid[0] === this.oponent) {
                 status = "YOU LOSE";
             }
-        }else if([this.grid[2], this.grid[5], this.grid[8]].every( (val, i, arr) => val === arr[0])){
-            if(this.grid[2] === this.player){
+        } else if ([this.grid[2], this.grid[5], this.grid[8]].every((val, i, arr) => val === arr[0])) {
+            if (this.grid[2] === this.player) {
                 status = "YOU WIN";
-            }else if(this.grid[0] === this.oponent){
+            } else if (this.grid[0] === this.oponent) {
                 status = "YOU LOSE";
             }
-        }else if(this.grid.every(el =>  el !== "")){
+        } else if (this.grid.every(el => el !== "")) {
             status = "DRAW";
         }
 
