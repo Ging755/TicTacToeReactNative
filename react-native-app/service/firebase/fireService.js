b41873e7c017ce22be6a2eb00b3eddf3efd99a2a
Ging755 <denis.milic2@gmail.com>
2020-04-25 11:26:39 +0200

    tic tac toe firebase config

--- react-native-app/service/firebase/fireService.js
+++ react-native-app/service/firebase/fireService.js
@@ -8,14 +8,14 @@ class Fire {
   init = () => {
     try {
       firebase.initializeApp({
-        apiKey: "",
-        authDomain: "",
-        databaseURL: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: "",
-        measurementId: ""
+        apiKey: "AIzaSyBr4UqHBBTl4lIrHIhvwjucRzbSdkug0Zw",
+        authDomain: "online-tic-tac-toe-51195.firebaseapp.com",
+        databaseURL: "https://online-tic-tac-toe-51195.firebaseio.com",
+        projectId: "online-tic-tac-toe-51195",
+        storageBucket: "online-tic-tac-toe-51195.appspot.com",
+        messagingSenderId: "1060489016860",
+        appId: "1:1060489016860:web:3f944a7178637271eb51d9",
+        measurementId: "G-N38P84G2JZ"
       });
     } catch (err) {
       // we skip the "already exists" message which is
